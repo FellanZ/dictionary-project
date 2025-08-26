c774a8916462ecd538fab44122369990df7dcab5
FellanZ <zimmerfellan@gmail.com>
2025-08-26 20:12:41 +0200

    Added search enginee

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      hello
+      <div className="container">
+        <header className="App-header"></header>
+        hello
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Felicia Z</small>
+        </footer>
+      </div>
     </div>
   );
 }
