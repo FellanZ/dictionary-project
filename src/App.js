031ee135e55d88819c9fa55f36b20da79d83034f
FellanZ <zimmerfellan@gmail.com>
2025-08-26 19:06:40 +0200

    Tested Bootstrap with button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      Hello
+      <a href="#" className="btn btn-primary">
+        Hello
+      </a>
     </div>
   );
 }
